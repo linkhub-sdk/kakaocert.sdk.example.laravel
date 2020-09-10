4ff638c4f2dc30094bc1db5bfe6208c0f1ba993e
JohnJeong123 <frenchofkiss@gmail.com>
2020-09-10 12:06:57 +0900

    added Verify API Comment

--- app/Http/Controllers/KakaocertController.php
+++ app/Http/Controllers/KakaocertController.php
@@ -142,6 +142,7 @@ class KakaocertController extends Controller
   /*
   * 자동이체 출금동의 서명을 검증합니다.
   * - 서명검증시 전자서명 데이터 전문(signedData)이 반환됩니다.
+  * - 카카오페이 서비스 운영정책에 따라 검증 API는 1회만 호출할 수 있습니다. 재시도시 오류처리됩니다.
   */
   public function VerifyCMS(){
 
@@ -260,6 +261,7 @@ class KakaocertController extends Controller
   * 본인인증 서명을 검증합니다.
   * - 서명검증시 전자서명 데이터 전문(signedData)이 반환됩니다.
   * - 본인인증 요청시 기재한 Token과 서명 검증시 반환되는 signedData의 동일여부를 확인하여 본인인증 검증을 완료합니다.
+  * - 카카오페이 서비스 운영정책에 따라 검증 API는 1회만 호출할 수 있습니다. 재시도시 오류처리됩니다.
   */
   public function VerifyAuth(){
 
@@ -380,6 +382,7 @@ class KakaocertController extends Controller
   /*
   * 전자서명 서명을 검증합니다.
   * - 서명검증시 전자서명 데이터 전문(signedData)이 반환됩니다.
+  * - 카카오페이 서비스 운영정책에 따라 검증 API는 1회만 호출할 수 있습니다. 재시도시 오류처리됩니다.
   */
   public function VerifyESign(){
 
